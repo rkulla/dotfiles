a8669e827c8bbff20f67730de8a1f3c86f874bd6
Ryan Kulla <rkulla@gmail.com>
2022-11-13 12:42:16 -0800

    Updates nvim

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -6,24 +6,25 @@ require('lualine').setup({
     globalstatus = true
   },
   sections = {
-    lualine_a = { 'branch' },       -- https://github.com/nvim-lualine/lualine.nvim/issues/888
+    lualine_a = { 'branch' },              -- https://github.com/nvim-lualine/lualine.nvim/issues/888
     lualine_b = {
       {
         'buffers',
-        show_filename_only = true,        -- Shows shortened relative path when set to false.
-        hide_filename_extension = false,  -- Hide filename extension when set to true.
-        show_modified_status = true,      -- Shows indicator when the buffer is modified.
+        show_filename_only = true,         -- Shows shortened relative path when set to false.
+        hide_filename_extension = false,   -- Hide filename extension when set to true.
+        show_modified_status = true,       -- Shows indicator when the buffer is modified.
         max_length = vim.o.columns * 2 / 3,
         mode = 4, 
 
         symbols = {
-          modified = ' ●',                -- Text to show when the buffer is modified
-          alternate_file = '^',           -- Text to show to identify the alternate file
-          directory =  '',               -- Text to show when the buffer is a directory
+          modified = ' ●',                 -- Text to show when the buffer is modified
+          alternate_file = '^',            -- Text to show to identify the alternate file
+          directory =  '',                -- Text to show when the buffer is a directory
         }, 
       }
     },
-   lualine_c = {},                        -- Don't show filename since I do in buffers
-   lualine_x = {'encoding', 'fileformat'} -- Don't show filetype since I use icons
-  }
+    lualine_c = {},                        -- Don't show filename since I do in buffers
+    lualine_x = {'encoding', 'fileformat'} -- Don't show filetype since I use icons
+  },
+  winbar = {}
 })
