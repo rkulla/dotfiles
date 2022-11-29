cfc4c5033535ac309bfa507677a68c3cae2a612f
Ryan Kulla <rkulla@gmail.com>
2022-11-29 14:39:07 -0800

    Updates nvim - stylua formatting

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -3,72 +3,70 @@
 -- Run :PackerSync to install/update the plugins from packer.startup below
 
 -- Custom Packer initialization
-packer = require 'packer'
-packer.init {
-  opt_default = false,  -- default to using start/ (as opposed to opt/)
+packer = require("packer")
+packer.init({
+  opt_default = false, -- default to using start/ (as opposed to opt/)
   display = {
-    open_fn = require('packer.util').float, -- Use float window, not split
+    open_fn = require("packer.util").float, -- Use float window, not split
     show_all_info = true, -- Show all update details automatically
-  }
-}
+  },
+})
 packer.reset()
 
 return packer.startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use("wbthomason/packer.nvim")
 
-  use 'neovim/nvim-lspconfig'
-  use 'jose-elias-alvarez/null-ls.nvim'
+  use("neovim/nvim-lspconfig")
+  use("jose-elias-alvarez/null-ls.nvim")
 
-  use 'folke/tokyonight.nvim'
+  use("folke/tokyonight.nvim")
 
   -- Plenary is required for certain plugins like Telescope
-  use 'nvim-lua/plenary.nvim'
+  use("nvim-lua/plenary.nvim")
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    opt = true
-  }
-  
+  use({
+    "nvim-telescope/telescope.nvim",
+    opt = true,
+  })
+
   -- requires telescope and `gh` cli
-  use 'nvim-telescope/telescope-github.nvim'
+  use("nvim-telescope/telescope-github.nvim")
 
-  use 'glepnir/dashboard-nvim'
+  use("glepnir/dashboard-nvim")
 
-  use {
+  use({
     "folke/which-key.nvim",
     config = function()
-      require("which-key").setup {
-      }
-    end
-  }
+      require("which-key").setup({})
+    end,
+  })
 
-  use 'tpope/vim-commentary'
+  use("tpope/vim-commentary")
 
-  use 'tpope/vim-surround'
+  use("tpope/vim-surround")
 
-  use 'tpope/vim-fugitive'
+  use("tpope/vim-fugitive")
   -- rhubarb enables :GBrowse from fugitive to open GitHub
-  use 'tpope/vim-rhubarb'
+  use("tpope/vim-rhubarb")
 
-  use 'lewis6991/gitsigns.nvim'
+  use("lewis6991/gitsigns.nvim")
 
-  use {
-    'nvim-lualine/lualine.nvim',
-  }
+  use("nvim-lualine/lualine.nvim")
 
   -- install markdown-preview without yarn or npm
   use({
     "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
-    opt = true
+    run = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    opt = true,
   })
 
-  use {
-    'nvim-tree/nvim-tree.lua',
+  use({
+    "nvim-tree/nvim-tree.lua",
     requires = {
-      'nvim-tree/nvim-web-devicons', 
-    }
-  }
-
+      "nvim-tree/nvim-web-devicons",
+    },
+  })
 end)
