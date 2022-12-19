e7db668c25ce5131f1d6b1be4bf38e697b3bf5e4
Ryan Kulla <rkulla@gmail.com>
2022-12-19 07:52:36 -0800

    Updates nvim - lsp

--- .config/nvim/lua/user/lsp.lua
+++ .config/nvim/lua/user/lsp.lua
@@ -23,6 +23,26 @@ local on_attach = function(client, bufnr)
 
   -- Enable the 'hints' section of gopls
   if client.name == "gopls" then ih.on_attach(client, bufnr) end
+
+  -- Highlight symbol under cursor everywhere
+  if client.server_capabilities.documentHighlightProvider then
+    vim.cmd([[
+      hi! LspReferenceRead cterm=bold ctermbg=235 guibg=LightYellow
+      hi! LspReferenceText cterm=bold ctermbg=235 guibg=LightYellow
+      hi! LspReferenceWrite cterm=bold ctermbg=235 guibg=LightYellow
+    ]])
+    vim.api.nvim_create_augroup("lsp_document_highlight", {})
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      group = "lsp_document_highlight",
+      buffer = 0,
+      callback = vim.lsp.buf.document_highlight,
+    })
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      group = "lsp_document_highlight",
+      buffer = 0,
+      callback = vim.lsp.buf.clear_references,
+    })
+  end
 end
 
 -- Golang: Auto-import / sort imports on save / format code
