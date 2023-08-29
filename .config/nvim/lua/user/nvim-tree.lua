a531126f5f57d190a5556e5d194289428bbf8eab
Ryan Kulla <rkulla@gmail.com>
2023-08-29 10:39:36 -0700

    Updates nvim null-ls config to modern version requirements

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -2,16 +2,27 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<cr>")
 vim.keymap.set("n", "<leader>nf", ":NvimTreeFindFile<cr>")
 vim.keymap.set("n", "<leader>nc", ":NvimTreeCollapse<cr>")
 
+local function my_on_attach(bufnr)
+  local api = require("nvim-tree.api")
+
+  local function opts(desc) return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true } end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set("n", "u", api.tree.change_root_to_parent, opts("Up"))
+  vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+end
+
 require("nvim-tree").setup({
   sort_by = "name",
   view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
+    width = table,
   },
+
+  on_attach = my_on_attach,
+
   renderer = {
     group_empty = true,
   },
