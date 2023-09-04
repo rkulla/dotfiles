4bbcf9d577205fada13f713d064cc518d74a5cdb
Ryan Kulla <rkulla@gmail.com>
2023-09-04 09:36:55 -0700

    Updates nvim indent guides

--- .config/nvim/lua/user/indent-blankline.lua
+++ .config/nvim/lua/user/indent-blankline.lua
@@ -1,7 +1,8 @@
 vim.keymap.set("n", "<leader>ti", ":IndentBlanklineToggle<cr>", { desc = "Toggle vertical indentation lines" })
 
--- Use diff chars if indented more than 4 levels, to say "GONE TOO FAR!"
-vim.g.indent_blankline_char_list = { "┃", "│", "│", "│", "┆", "┊" }
+-- Use diff chars if indented more than 3 levels, to warn if "GONE TOO FAR!"
+-- I make exceptions if it's an object literal, but this is still a good guideline
+vim.g.indent_blankline_char_list = { "┃", "│", "│", "┆", "┆", "┊" }
 
 -- require("indent_blankline").setup({
 -- })
