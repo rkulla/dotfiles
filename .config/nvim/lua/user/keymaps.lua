72fe9301850d92b94c79a61105451d8ea2145db9
Ryan Kulla <rkulla@gmail.com>
2023-09-20 14:37:55 -0700

    Updates fugitive map

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -119,4 +119,4 @@ end, { desc = "Winbar Hide" })
 -- Fugitive
 map("n", "gst", "<cmd>G status<cr>", { desc = "Git status" })
 map("n", "gsf", "<cmd>Gclog -n 1 --follow --diff-filter=A -- %<cr>", { desc = "Git [curr file] first added" })
-map("n", "gsa", "<cmd>Gclog --pretty=reference -- %<cr>", { desc = "Git all commits [curr file]" })
+map("n", "gsa", '<cmd>Gclog --date=short --pretty=format:"%h ┃ %cd ┃ %an ┃ %s" -- %<cr>', { desc = "Git all commits [curr file]" })
