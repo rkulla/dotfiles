07e7322d314b050e3dec0a2e51bc70ac00d7d6c6
Ryan Kulla <rkulla@gmail.com>
2025-04-11 07:46:32 -0700

    nvim refactor

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -6,19 +6,34 @@ local M = {}
 
 -- Highlight overrides
 local function MyHighlights()
+  local normal = vim.api.nvim_get_hl(0, { name = "Normal" }) or {}
+  local cursor_bg = "#819090"
+
   -- Make window borders visible
-  vim.api.nvim_set_hl(0, "WinSeparator", { fg = "NONE", bg = "bg", bold = true })
+  vim.api.nvim_set_hl(0, "WinSeparator", { fg = "NONE", bg = normal.bg, bold = true })
 
   -- Enable cursorline
   vim.o.cursorline = true
 
-  -- Cursor line highlights
-  vim.api.nvim_set_hl(0, "CursorLine", { bold = true })
-  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#819090", bg = "NONE" })
+  -- Line number on cursorline
+  vim.api.nvim_set_hl(0, "CursorLineNr", {
+    fg = "#819090", -- you can change this
+    bg = "NONE",
+    bold = true,
+    italic = false, -- cursorline won't be italic to distinguish it
+  })
 
   -- Italic line numbers
   vim.api.nvim_set_hl(0, "LineNr", { italic = true })
 end
+
+-- Run my highlights immediately
+MyHighlights()
+-- run MyHighlights() after colorscheme changes
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = MyHighlights,
+})
 
 -- Apply dark theme
 function M.Dark()
@@ -35,12 +50,6 @@ function M.Light()
   -- setFlashLight()
   MyHighlights()
 end
-
--- Autocommand: run MyHighlights() after colorscheme changes
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "*",
-  callback = MyHighlights,
-})
 
 -- Create user commands
 vim.api.nvim_create_user_command("Dark", function() M.Dark() end, {})
--- .config/nvim/lua/user/notify.lua
+++ .config/nvim/lua/user/notify.lua
@@ -1,5 +1,6 @@
 require("notify").setup({
-  background_colour = "NotifyBackground",
+  -- background_colour = "NotifyBackground",
+  background_colour = "#000000",
   fps = 30,
   icons = {
     DEBUG = "",
