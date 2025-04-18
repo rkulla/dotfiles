85bcb3494219c5c40f2e5df194cdf6860fed02d0
Ryan Kulla <rkulla@gmail.com>
2025-04-18 16:49:39 -0700

    Updates nvim

--- .config/nvim/ftplugin/go.lua
+++ .config/nvim/ftplugin/go.lua
@@ -1,6 +1,13 @@
+-- this is in ftplugin/ to only lazyload these keymaps when files are loaded, and just .go files
 local map = vim.keymap.set
 
-map("n", "<Leader>rr", ":!go run %<cr>", { desc = "Run current Go file" })
+map(
+  "n",
+  "<Leader>rr",
+  function() vim.cmd("botright new | setlocal nobuflisted | terminal clear && go run " .. vim.fn.expand("%")) end,
+  { desc = "Run current Go file in fresh terminal" }
+)
+
 map("n", "<Leader>rb", ":!go build %<cr>", { desc = "Build current Go file" })
 map("n", "<Leader>rT", ":!go test -coverprofile=coverage.out --count=1 -v ./...<cr>", { desc = "Test all Go test files" })
 map(
